1e2cc008d3d4286321ec82c654ae66657e927777
galt <galtfilin@gmail.com>
2025-02-26 23:06:21 +0000

    Ledger API type

---
+++ internal/app/webserver/api/transaction.go
@@ -0,0 +1,20 @@
+package ledger
+
+import (
+	"teya_home_assignment/internal/pkg/ledger"
+
+	"github.com/google/uuid"
+	"github.com/shopspring/decimal"
+)
+
+type Transaction struct {
+	ID     uuid.UUID       `json:"id"`
+	Amount decimal.Decimal `json:"amount"`
+}
+
+func FromTransactionModel(transaction ledger.Transaction) Transaction {
+	return Transaction{
+		ID:     transaction.ExternalID,
+		Amount: transaction.Amount,
+	}
+}
